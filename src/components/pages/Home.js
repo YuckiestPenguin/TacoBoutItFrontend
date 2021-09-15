b20d31d2a096de7859a041ee1788514359234ccc
Jesse Orozco <orozcojesse0@gmail.com>
2021-09-14 21:18:19 -0400

    Added Socail Media To Home Page

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,5 +1,6 @@
 import React from "react";
 import TacoPaco from "../../img/tacopaco.png";
+import SocialFollow from "../../components/SocialFollow";
 import "../../App.css";
 
 // const styles = {
@@ -15,9 +16,12 @@ import "../../App.css";
 
 export default function Home() {
   return (
-    <div>
-      <img src={TacoPaco} alt="TacoPaco" width="100%" height="100%" />
-      <figcaption>TacoBoutIT</figcaption>
-    </div>
+    <>
+      <div>
+        <img src={TacoPaco} alt="TacoPaco" width="100%" height="100%" />
+        <figcaption>TacoBoutIT</figcaption>
+      </div>
+      <SocialFollow />
+    </>
   );
 }
