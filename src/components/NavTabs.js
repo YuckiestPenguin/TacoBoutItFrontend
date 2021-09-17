8801e04701ffc7a932d200e088dec827ac841c31
Jesse Orozco <orozcojesse0@gmail.com>
2021-09-16 20:10:23 -0400

    adjusted navbar css

--- src/components/NavTabs.js
+++ src/components/NavTabs.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Nav from "../components/styles/navbar.css";
 
 // Here we are using object destructuring assignment to pluck off our variables from the props object
 // We assign them to their own variable names
